1e7cada4af4bda5e3d752ca52db507671403a164
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-08-14 16:43:55 +0100

    References and initial structure

--- src/server/GridBeyond.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GirdBeyond.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/server/GridBeyond.Domain/Entities/Data.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GridBeyond.Domain.Entities
+{
+    public class Data
+    {
+        public DateTime Date { get; set; }
+        public double MarketpriceEX1 { get; set; }
+    }
+}
