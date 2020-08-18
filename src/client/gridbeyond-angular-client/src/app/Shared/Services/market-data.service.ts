2e1020f76275981c60d0e9b72ab794a72ba8dad6
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-08-18 11:00:10 +0100

    angular http service

---
+++ src/client/gridbeyond-angular-client/src/app/Shared/Services/market-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IReportDataModel } from '../Models/IReportDataModel';
+import { IInsertedRecordsModel } from '../Models/IInsertedRecordsModel';
+import { IMarketDataModel } from '../Models/IMarketDataModel';
+
+import clientConfig from '../../../assets/config.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarketDataService {
+  constructor(private client: HttpClient) {
+  }
+
+  public GetReportData(): Observable<IReportDataModel> {
+    return this.client.get(`${clientConfig.api.baseUrl}/marketdata/report`);
+  }
+
+  public GetAllData(): Observable<IMarketDataModel> {
+    return this.client.get(`${clientConfig.api.baseUrl}/marketdata`);
+  }
+
+  public InsertRecords(records: string[]): Observable<IInsertedRecordsModel> {
+    return this.client.post(`${clientConfig.api.baseUrl}/marketdata`, records);
+  }
+}
