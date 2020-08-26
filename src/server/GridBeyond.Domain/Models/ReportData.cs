452d49b63f5ff54a7f92d8686a015daf526d6ddc
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-08-26 17:15:27 +0100

    ReportData Model Tidy up

--- src/server/GridBeyond.Domain/Models/ReportData.cs
+++ src/server/GridBeyond.Domain/Models/ReportData.cs
@@ -7,18 +7,21 @@ namespace GridBeyond.Domain.Models
     public class ReportData
     {
         public DateTime? HighestValueDate { get; set; }
-        public double HighestValue { get; set; }
+        public PeakQuiet HighestPeak => HighestValueDate.HasValue
+            ? PeakQuietPerDate.Single(x => x.Date.Date == HighestValueDate.Value.Date)
+            : null;
+
         public DateTime? LowestValueDate { get; set; }
+        public PeakQuiet QuieterHour => LowestValueDate.HasValue
+            ? PeakQuietPerDate.Single(x => x.Date.Date == LowestValueDate.Value.Date)
+            : null;
+
+        public double HighestValue { get; set; }
         public double LowestValue { get; set; }
         public double AverageValue { get; set; }
         public int TotalRecords { get; set; }
         public List<PeakQuiet> PeakQuietPerDate { get; set; }
-        public PeakQuiet HighestPeak => HighestValueDate.HasValue
-            ? PeakQuietPerDate.Single(x => x.Date.Date == HighestValueDate.Value.Date)
-            : null;
-        public PeakQuiet QuieterHour => LowestValueDate.HasValue
-            ? PeakQuietPerDate.Single(x => x.Date.Date == LowestValueDate.Value.Date)
-            : null;
+        
     }
 
     public class PeakQuiet
