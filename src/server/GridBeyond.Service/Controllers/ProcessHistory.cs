2a56d9d6d5523b936507fc9187ed4bc76dc241d6
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-08-27 02:33:13 +0100

    ProcessHistory Controller

---
+++ src/server/GridBeyond.Service/Controllers/ProcessHistory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GridBeyond.Domain.Interfaces.Services;
+using GridBeyond.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GridBeyond.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProcessHistory : Controller
+    {
+        private readonly IProcessHistoryService _service;
+
+        public ProcessHistory(IProcessHistoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<List<ProcessHistoryModel>> Get()
+        {
+            return await _service.GetProcessHistory();
+        }
+    }
+}
